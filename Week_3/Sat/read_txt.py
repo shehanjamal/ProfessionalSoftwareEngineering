3a397b4d220f65bf9cf43377c7a39ac462e93aca
Shehan Jamal <58282749+shehanjamal@users.noreply.github.com>
2025-08-16 12:01:19 +1200

    added class

--- Week_3/Sat/read_txt.py
+++ Week_3/Sat/read_txt.py
@@ -1,16 +1,24 @@
-def read_txt():
-    with open("demo.txt", "r", encoding="utf-8") as file:
-        content = file.readlines()
-        for line in content:
-            print(line)
+class ReadWriteTxt:
+    def __init__(self, txt_add, filename):
+        self.text_add = txt_add
+        self.filename = filename
 
-def write_txt():
-    with open("demo.txt", "a") as file:
-        file.write("This is as addition done by user.\n")
+    def read_txt(self):
+        with open(self.filename, "r", encoding="utf-8") as file:
+            content = file.readlines()
+            for line in content:
+                print(line)
+
+    def write_txt(self):
+        with open(self.filename, "a") as file:
+            file.write(self.text_add)
+
+
 
 def main():
-    read_txt()
-    write_txt()
-    
+    read_write = ReadWriteTxt("This is an addition done by user.\n", "demo.txt")
+    read_write.read_txt()
+    read_write.write_txt()
+
 if __name__ == "__main__":
     main()
