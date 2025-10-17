e9b65cd38c5593282d9245c981d3f2fdf856f6f7
Shehan Jamal <58282749+shehanjamal@users.noreply.github.com>
2025-10-18 12:01:42 +1300

    link and image

--- Week_12/flask_activity.py
+++ Week_12/flask_activity.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -17,6 +17,13 @@ def data(name):
 @app.route('/cal/<int:number>')
 def show_square(number):
     return f"The square of {number} is {number**2}"
+
+@app.route('/link')
+def link():
+    image_url = url_for('static', filename='koi-fish.png')
+    return '<h1><a href="https://pixers.co.nz/posters/koi-fish-ying-yang-symbol-102244746">This is a link</a></h1>' \
+    f'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTBX3F8XrenPAlRbSvx9-c07_o15gkUUB8Rw&s" alt="Koi Fish Ying Yang Symbol" loading="lazy"/>'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
