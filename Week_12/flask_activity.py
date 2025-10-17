96ce11c98a996fa8d4c3ab7a8babda550ab56d05
Shehan Jamal <58282749+shehanjamal@users.noreply.github.com>
2025-10-18 11:29:54 +1300

    acitivity 2 and 3

--- Week_12/flask_activity.py
+++ Week_12/flask_activity.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return "<h1>Hello Flask</h1>"
 
+@app.route("/bye")
+def bye():
+    return "<h1>Goodbye Flask</h1>"
+
+@app.route("/username/<name>")
+def data(name):
+    return f"<h1>Hello {name} is learning flask.</h1>"
+
+@app.route('/cal/<int:number>')
+def show_square(number):
+    return f"The square of {number} is {number**2}"
+
 if __name__ == "__main__":
     app.run(debug=True)
