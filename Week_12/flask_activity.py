3e9a212c2cf8510044f85a39499d68c4082af836
Shehan Jamal <58282749+shehanjamal@users.noreply.github.com>
2025-10-19 08:43:00 +1300

    css added

--- Week_12/flask_activity.py
+++ Week_12/flask_activity.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
@@ -20,10 +20,31 @@ def show_square(number):
 
 @app.route('/link')
 def link():
-    image_url = url_for('static', filename='koi-fish.png')
     return '<h1><a href="https://pixers.co.nz/posters/koi-fish-ying-yang-symbol-102244746">This is a link</a></h1>' \
     f'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTBX3F8XrenPAlRbSvx9-c07_o15gkUUB8Rw&s" alt="Koi Fish Ying Yang Symbol" loading="lazy"/>'
 
+@app.route("/home_page")
+def home_page():
+    return render_template("./index.html")
+
+@app.route('/temperature_calculator/<int:temp_1>/<conversion_type>')
+def temperature_calculator(temp_1, conversion_type):
+    if conversion_type == "c_to_f":
+        converted_temp = (temp_1 * 9/5) + 32
+    elif conversion_type == "f_to_c":
+        converted_temp = (temp_1 - 32) * 5/9
+    elif conversion_type == "c_to_k":
+        converted_temp = temp_1 + 273.15
+    elif conversion_type == "k_to_c":
+        converted_temp = temp_1 - 273.15
+    elif conversion_type == "f_to_k":
+        converted_temp = (temp_1 - 32) * 5/9 + 273.15
+    elif conversion_type == "k_to_f":
+        converted_temp = (temp_1 - 273.15) * 9/5 + 32
+    else:
+        return jsonify({"error": "Invalid conversion type"}), 400
+
+    return jsonify({"converted_temp": round(converted_temp, 2)})
 
 if __name__ == "__main__":
     app.run(debug=True)
